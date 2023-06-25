e064c5ea86750b40d4952521a348f5a660787750
canoel22 <carine.noel2003@gmail.com>
2023-06-25 19:26:36 -0300

    com testes + time.h

--- backtrackingSudoku.c
+++ backtrackingSudoku.c
@@ -2,6 +2,7 @@
 #include <time.h>
 
 #define TAM 9 //tamanho da matriz
+int operationCount = 0;
 
 //-------------------- inicializa a matriz -------------------------
 
@@ -15,7 +16,7 @@ void inicializaGrade(int grade[TAM][TAM]){
 
 //------------------ procura um lugar vazio --------------------------
 
-int LugarVazio(int grade[TAM][TAM], int *linha, int *coluna){
+int lugarVazio(int grade[TAM][TAM], int *linha, int *coluna){
 	for ( ;(*linha) < TAM; (*linha)++){
 		for (;(*coluna) < TAM; (*coluna)++)
 			if (grade[(*linha)][(*coluna)] == 0)
@@ -28,7 +29,7 @@ int LugarVazio(int grade[TAM][TAM], int *linha, int *coluna){
 
 //-------------- verifica se o número ja está na linha ----------------------
 
-int RepetidoLinha(int grade[TAM][TAM], int linha, int num){
+int repetidoLinha(int grade[TAM][TAM], int linha, int num){
 	for (int coluna = 0; coluna < TAM; coluna++)
 		if (grade[linha][coluna] == num) return 1;
 	return 0;
@@ -36,7 +37,7 @@ int RepetidoLinha(int grade[TAM][TAM], int linha, int num){
 
 //-------------- verifica se o número ja está na coluna ----------------------
 
-int RepetidoColuna(int grade[TAM][TAM], int coluna, int num){
+int repetidoColuna(int grade[TAM][TAM], int coluna, int num){
 	for (int linha = 0; linha < TAM; linha++)
 		if (grade[linha][coluna] == num) return 1;	
 	return 0;
@@ -44,32 +45,30 @@ int RepetidoColuna(int grade[TAM][TAM], int coluna, int num){
 
 //------------- verifica se o número ja está no grupo 3x3 ----------------------
 
-int RepetidoGrupo(int grade[TAM][TAM], int LinhaInicial,int ColunaInicial, int num){
+int repetidoGrupo(int grade[TAM][TAM], int LinhaInicial,int ColunaInicial, int num){
 	for (int linha = 0; linha < 3; linha++)
 		for (int coluna = 0; coluna < 3; coluna++)
-			//se sim,retorna 1
 			if (grade[linha + LinhaInicial][coluna + ColunaInicial] == num)
 				return 1;
-	//else retorna 0
 	return 0;
 }
 
 //----------- verifica a validade do numero: se já está na linha/coluna/grupo -------------
 
-int Valido(int grade[TAM][TAM], int linha,int coluna, int num){
-	return !RepetidoLinha(grade, linha, num)
-		&& !RepetidoColuna(grade, coluna, num)
-		&& !RepetidoGrupo(grade, linha - linha % 3,coluna - coluna % 3, num);
+int valido(int grade[TAM][TAM], int linha,int coluna, int num){
+	return !repetidoLinha(grade, linha, num)
+		&& !repetidoColuna(grade, coluna, num)
+		&& !repetidoGrupo(grade, linha - linha % 3,coluna - coluna % 3, num);
 }
 
 //----------------------- resolve o Sudoku --------------------------------
 
 int resolveSudoku(int grade[TAM][TAM],int ordem[],int linha,int coluna){
-	if (!LugarVazio(grade, &linha, &coluna)) //se todos os lugares estão cheios, chegamos no resultado
+	if (!lugarVazio(grade, &linha, &coluna)) //se todos os lugares estão cheios, chegamos no resultado
 		return 1;
 		
 	for (int num = 0; num < TAM; num++){
-		if (Valido(grade, linha, coluna, ordem[num])){
+		if (valido(grade, linha, coluna, ordem[num])){
 			grade[linha][coluna] = ordem[num];
 			if(resolveSudoku(grade,ordem,linha,coluna+1)){
 				return 1;
@@ -105,12 +104,24 @@ void printaGrade(int grade[TAM][TAM]){
 //------------------------- main -------------------------------
 
 int main(){
+	clock_t start, end;
+    double executionTime;
+    
+    start = clock(); // inicia a contagem do tempo
+
     int grade[TAM][TAM];
 	int ordem1[] = {1,2,3,4,5,6,7,8,9};
 
     inicializaGrade(grade);
-    resolveSudoku(grade, ordem1, 0, 0);
-    printaGrade(grade);
+    if (resolveSudoku(grade, ordem1, 0, 0)){
+		end = clock(); // Finaliza a contagem do tempo
+        executionTime = ((double) (end - start)) / CLOCKS_PER_SEC * 1000; // Tempo de execução em milissegundos
+        printaGrade(grade);
+        printf("\nNúmero de operações: %d\n", operationCount);
+        printf("Tempo de execução: %.2f ms\n", executionTime);
+	} else {
+        printf("Não é possível resolver o Sudoku.\n");
+    }
 
     return 0;
 }
